58507bea326f84df8818663195d9c3b6bad22a39
TBogatinoska <tea.bogatinoska@endava.com>
2024-07-19 12:04:08 +0200

    Initial commit

---
+++ src/main/java/com/example/WeatherForecastingApp/authentication/model/ERole.java
@@ -0,0 +1,7 @@
+package com.example.WeatherForecastingApp.authentication.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/example/WeatherForecastingApp/authentication/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.WeatherForecastingApp.authentication.repository;
+
+import com.example.WeatherForecastingApp.authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
